913c6287e97aaebee34118297ee441193b48b3f1
Tom <139076122+momotot@users.noreply.github.com>
2023-12-27 21:13:42 +0100

    Delete src/Start.bat

--- src/Start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& {Start-Process python -ArgumentList '%~dp0Launcher.py' -WindowStyle Hidden -Verb RunAs}"
